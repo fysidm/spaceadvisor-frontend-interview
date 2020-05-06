d16ff0394ed5959bc4f2afcc7a3ef07aa2e1da51
GeorgeWu <fysidm@gmail.com>
2020-05-07 01:15:13 +0800

    Updates

---
+++ precache-manifest.c589bc60380524763cf902bfa1054315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad32459d78bad347dfb95cb89dd5711",
+    "url": "/spaceadvisor-frontend-interview/index.html"
+  },
+  {
+    "revision": "82e8aaa5f7c37ad2f5ae",
+    "url": "/spaceadvisor-frontend-interview/static/css/main.7f88802c.chunk.css"
+  },
+  {
+    "revision": "f7ccfd6199f814a6c6a0",
+    "url": "/spaceadvisor-frontend-interview/static/js/2.f1971f92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spaceadvisor-frontend-interview/static/js/2.f1971f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e8aaa5f7c37ad2f5ae",
+    "url": "/spaceadvisor-frontend-interview/static/js/main.d34c9947.chunk.js"
+  },
+  {
+    "revision": "6d9904fa3883f2e6fc65",
+    "url": "/spaceadvisor-frontend-interview/static/js/runtime-main.f76bd704.js"
+  }
+]);
